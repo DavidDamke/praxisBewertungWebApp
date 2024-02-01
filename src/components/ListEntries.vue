9c907e6156f7d83e0ac204dec4b25f6bd21c9489
David Damke <dd-192057@rwu.de>
2024-02-01 12:15:17 +0100

    add function to calculate avgs

--- src/components/ListEntries.vue
+++ src/components/ListEntries.vue
@@ -3,9 +3,11 @@
     <v-list>
       <v-list-item-group>
         <v-list-item v-for="company in companies" :key="company._id">
-          <v-list-item-content>
-            <v-list-item-title>{{ company.name }}</v-list-item-title>
-            <v-list-item-subtitle>{{ company.industry }} - {{ company.location }}</v-list-item-subtitle>
+          <v-list-item-content id="companyContentList">
+            <v-list-item-content>{{ company.name }}</v-list-item-content>
+            <v-list-item-content><v-rating  id="rating" :model-value="avgRating(company)" readonly></v-rating></v-list-item-content>
+         
+            <!-- <v-list-item-subtitle>{{ company.industry }} - {{ company.location }}</v-list-item-subtitle> -->
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
@@ -18,17 +20,35 @@
 
 export default {
 
+  methods: {
+    avgRating(company) {
+      var sumratings = 0;
+      for (var i = 0; i < company.ratings.length; i++){
+        sumratings += company.ratings[i].score;
+      }
+            console.log(sumratings);
+
+      return sumratings/company.ratings.length;
+      }
+    },
   data() {
     return {
       companies: [],
+      ratingValue: 0
     };
   },
   mounted() {
     axios.get('http://localhost:8080/getAllCompanies').then(response => {
       this.companies = response.data;
-      console.log(response.data);
     }).catch(error => console.error('Error:', error));
 
   },
+ 
 }
 </script>
+<style scoped>
+#companyContentList {
+    display: flex;
+}
+
+</style>
