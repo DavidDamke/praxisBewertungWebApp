647162052913d54c576b2da73e48cb009caa75e7
David Damke <dd-192057@rwu.de>
2024-02-03 13:03:01 +0100

    add search comonent

---
+++ src/components/Search.vue
@@ -0,0 +1,40 @@
+<template>
+ <v-card
+    color="grey-lighten-4"
+    flat
+    rounded="0"
+  >
+    <v-toolbar>
+        <v-text-field label="Search" type="text" clearable  append-inner-icon="mdi-magnify"
+        @click:append-inner="onSearch"
+        v-model="searchInput"
+></v-text-field>
+    
+    </v-toolbar>
+  </v-card></template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchInput: '',
+        };
+    },
+    watch: {
+        searchInput(newVal, oldVal) {
+            console.log(newVal); // This logs the current value of searchInput whenever it changes
+        }
+    },
+    methods: {
+        onSearch() {
+            console.log(this.searchInput);
+        }
+        
+    }
+
+}
+</script>
+
+<style>
+
+</style>
